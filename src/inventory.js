74d719f53057465c252ee5a87b5ff8ef00fd511d
pdavisDU <pdavisdu@gmail.com>
2022-12-02 11:59:43 -0700

    changed stock to quantity

---
+++ src/inventory.js
@@ -0,0 +1,8 @@
+// we need to create an inventory object to store our items
+
+const inventory = {
+  //we create an empty array to store our items in
+  items: [],
+  //addItem is a method thatwe use to add a new item to the inventory. passing in name, quantity, and photo as arguments.
+  addItem(name, quantity, photo) {},
+};
