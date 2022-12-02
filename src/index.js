1db36dd9c35798df62ce1d9da5c982d7600c12e4
pdavisDU <pdavisdu@gmail.com>
2022-12-02 13:06:36 -0700

    added function to render inventory and looping through to create a list

--- src/index.js
+++ src/index.js
@@ -14,4 +14,36 @@ form.addEventListener("submit", (event) => {
   const name = document.querySelector("#input-name").value;
   const quantity = document.querySelector("#input-quantity").value;
   const photo = document.querySelector("#input-photo").files[0];
+
+  //add the item to the inventory
+  inventoryList.addItem(name, quantity, photo);
+
+  //clear form fields
+  form.reset();
+  //Render the inventory list
+  renderInventory();
 });
+
+//create the renderInventory function
+function renderInventory() {
+  //get items from the inventory
+  const items = inventory.getInventory();
+
+  //clear the inventory list
+  inventoryList.innerHTML = "";
+
+  //loop through the items to render them to the page
+  for (const item of items) {
+    //create a list item element
+    const li = document.createElement("li");
+
+    //set the inner html of the list item to the name and quantity of the item
+    li.innerHTML = `${item.name}: ${item.quantity}`;
+
+    //append the list item element to the inventory list
+    inventoryList.appendChild(li);
+  }
+}
+
+//render the initial inventory
+renderInventory();
