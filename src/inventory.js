964edd7428489f0e76e697e4f7d41ca7842cb799
pdavisDU <pdavisdu@gmail.com>
2022-12-02 12:08:19 -0700

    adding .push to push items into array

--- src/inventory.js
+++ src/inventory.js
@@ -4,5 +4,12 @@ const inventory = {
   //we create an empty array to store our items in
   items: [],
   //addItem is a method thatwe use to add a new item to the inventory. passing in name, quantity, and photo as arguments.
-  addItem(name, quantity, photo) {},
+  addItem(name, quantity, photo) {
+    //need to be able to push a new object with specified properties to the items array
+    this.items.push({
+      name,
+      quantity,
+      photo,
+    });
+  },
 };
